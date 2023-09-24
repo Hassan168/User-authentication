f30ea9bfbc4f2475f8038e662998f5270556a15f
unknown <Hassan Bashir>
2023-09-24 23:22:18 +0500

    first push

---
+++ BE/routes/userRouter.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const middleware = require("../middleware");
+
+const userController = require("../controllers/userController");
+
+router.post("/createUser", userController.createUser);
+router.get("/getAllUsers", userController.getAllUsers);
+router.get("/getUserByEmail", userController.getUserByEmail);
+router.put("/updateUser", userController.updateUser);
+router.delete("/deleteUser", userController.deleteUser);
+////////////////////////////////////
+router.post("/email", userController.mailservice);
+////////////////////////////////////
+
+module.exports = router;
